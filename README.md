22bf34c84d54c45af7b324ab814a758bb9c44280
Jayson Potter <iam@jaysonpotter.com>
2013-03-10 14:04:40 -0700

    updated readme, linted the script a bit, added link back to github commit

--- README.md
+++ README.md
@@ -3,11 +3,18 @@ JoiStyk
 
 *View a demo of [JoiStyk](http://projects.jaysonpotter.com/JoiStyk/)!*
 
+Sunday March 10, 2013 : 
+* did a BIT of JSLinting
+* changed the follow behavior, now follows the user 1:1 instead of a lag (seemed like a cool idea, but defeated the purpose to the advantage of the joystick following the user)
+* changed the basic colors
+* added some meta tags with charset and description
+* added github fork me ribbon
+
 Saturday March 9, 2013 : 
 This is a working prototype of an HTML5 canvas joystick with some key features. 
 * Can be used on mouse or touch event driven devices
 * JoiStyk is visible when the user is interacting with the canvas, and gone when the user is not
-* JoiStyk will adjust to when the user has gone farther than the initial bounds of the pad area.
+* JoiStyk adjusts to when the user has gone farther than the initial bounds of the pad area.
 * Basic output of X and Y coords
 
 Some things I'll be adding to JoiStyk are : 
@@ -16,4 +23,6 @@ Some things I'll be adding to JoiStyk are :
 * a circular joystick pad collision area in which JoiStyk will be bound to, just a box now
 * the ability to use images instead of circles
 * color controls
+
+Dropped features :
 * add velocity to how JoiStyk follows the pointer
--- _ui/js/JoiStyk.js
+++ _ui/js/JoiStyk.js
@@ -1,51 +1,14 @@
 window.onload = function () {
-	
+
 	// Yes they're global, yes it's gross. A lot is. I'm working on it. This is my playground. Thank you for checking it out
-	
+
 	// All the touchy and interactive
 	var try_touch;
 	var tap;
 	var tapEnd;
-	var tapCancel;
-	var tapEnd;
 	var tapMove;
-	
-	// Stores the info about the JoiStyk drawing elements
-	var padPos;
-	var stickPos;
-	var trackPos;
-	var padRad = 80;
-	var stickRad = 40;
-	var trackRad = 40; // <- this will need to go
-	var followSpeed = 1;
-	
-	// Canvas selection, size, and context
-	var canvas = document.getElementById('JoiStyk');
-  		canvas.width = 300;
-  		canvas.height = 300;
-  	var ctx = canvas.getContext('2d');
-  	
-  	// Pixel density info gathering
-  	var pixelRatio = window.devicePixelRatio;
-  	var backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
-                            ctx.mozBackingStorePixelRatio ||
-                            ctx.msBackingStorePixelRatio ||
-                            ctx.oBackingStorePixelRatio ||
-                            ctx.backingStorePixelRatio || 1;
-    var ratio = devicePixelRatio / backingStoreRatio;
-    
-    // If there are double or more pixels, scale the drawing times that ratio and stuff it back into the original dimensions
-  	if (pixelRatio >= 2) {
-  		var oldWidth = canvas.width;
-        var oldHeight = canvas.height;
-
-        canvas.width = oldWidth * ratio;
-        canvas.height = oldHeight * ratio;
-
-        canvas.style.width = oldWidth + 'px';
-        canvas.style.height = oldHeight + 'px';
-  		ctx.scale(ratio, ratio);
-  	}
+	var tapLeave;
+	var tapCancel;
 	
 	// If thou hast nay touch thy shall be treated as rattus
 	try {
@@ -70,6 +33,76 @@ window.onload = function () {
 	}
 	// END Detect the touch
 	
+	// Gets the position of the mouse or single finger
+  	var getPosition = function(canvas, evt) {
+  		evt.preventDefault();
+		var rect = canvas.getBoundingClientRect();
+		if(try_touch === 1){
+			return {
+				// this only tracks a single touch
+				x: evt.targetTouches[0].clientX - rect.left,
+				y: evt.targetTouches[0].clientY - rect.top
+			};
+		} else {
+			return {
+				x: evt.clientX - rect.left,
+				y: evt.clientY - rect.top
+			};
+		}
+	}
+	
+	// Stores the info about the JoiStyk drawing elements
+	var padPos;
+	var stickPos;
+	var trackPos;
+	var padRad = 80;
+	var stickRad = 40;
+	
+	// Canvas selection, size, and context
+	var canvas = document.getElementById('JoiStyk');
+  		canvas.width = 300;
+  		canvas.height = 300;
+  	// START Events
+		canvas.addEventListener(tapMove, function(evt) {
+			stickPos = getPosition(canvas, evt);
+			trackPos = getPosition(canvas, evt);
+		}, false);
+		
+		canvas.addEventListener(tap, function(evt) {
+			padPos = getPosition(canvas, evt);
+			stickPos = getPosition(canvas, evt);
+			trackPos = getPosition(canvas, evt);
+		}, false);
+		
+		canvas.addEventListener(tapEnd, function(evt){
+			stickPos = null;
+			padPos = null;
+		}, false);
+	//END Events
+  	var ctx = canvas.getContext('2d');
+  	
+  	// Pixel density info gathering, or default to 1
+  	var pixelRatio = window.devicePixelRatio || 1;
+  	var backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
+                            ctx.mozBackingStorePixelRatio ||
+                            ctx.msBackingStorePixelRatio ||
+                            ctx.oBackingStorePixelRatio ||
+                            ctx.backingStorePixelRatio || 1;
+    var ratio = pixelRatio / backingStoreRatio;
+    
+    // If there are double or more pixels, scale the drawing times that ratio and stuff it back into the original dimensions
+  	if (pixelRatio >= 2) {
+  		var oldWidth = canvas.width;
+        var oldHeight = canvas.height;
+
+        canvas.width = oldWidth * ratio;
+        canvas.height = oldHeight * ratio;
+
+        canvas.style.width = oldWidth + 'px';
+        canvas.style.height = oldHeight + 'px';
+  		ctx.scale(ratio, ratio);
+  	}
+	
 	// requestAnim shim layer by Paul Irish <- Crafty devil
 	window.requestAnimFrame = (function(){
 		return window.requestAnimationFrame		  || 
@@ -81,46 +114,6 @@ window.onload = function () {
 				   window.setTimeout(callback, 1000 / 60);
 			   };
 	}()); // <-- tucked in the, as Crockford says it, dog balls
-
-  	// Gets the position of the mouse or single finger
-  	var getPosition = function(canvas, evt) {
-  		evt.preventDefault();
-		var rect = canvas.getBoundingClientRect();
-		if(try_touch === 1){
-			return {
-				// this only tracks a single touch
-				x: evt.targetTouches[0].clientX - rect.left,
-				y: evt.targetTouches[0].clientY - rect.top
-			}
-		} else {
-			return {
-				x: evt.clientX - rect.left,
-				y: evt.clientY - rect.top
-			}
-		}
-	}
-	
-	// START Events
-	canvas.addEventListener(tapMove, function(evt) {
-		
-		stickPos = getPosition(canvas, evt);
-		trackPos = getPosition(canvas, evt);
-	
-	}, false);
-	canvas.addEventListener(tap, function(evt) {
-		
-		padPos = getPosition(canvas, evt);
-		stickPos = getPosition(canvas, evt);
-		trackPos = getPosition(canvas, evt);
-	
-	}, false);
-	canvas.addEventListener(tapEnd, function(evt){
-		
-		stickPos = null;
-		padPos = null;
-		
-	}, false);
-	//END Events
 	
 	//START Shapes
 	var drawJSPad = function() {
@@ -135,15 +128,7 @@ window.onload = function () {
 		ctx.beginPath();
 		ctx.arc(stickPos.x, stickPos.y, stickRad, 0, 2 * Math.PI, false);
 		ctx.closePath();
-		ctx.fillStyle = 'rgb(255, 255, 255)';
-		ctx.fill();
-	}
-	
-	var drawFollowDot = function() {
-		ctx.beginPath();
-		ctx.arc(trackPos.x, trackPos.y, trackRad, 0, 2 * Math.PI, false);
-		ctx.closePath();
-		ctx.fillStyle = 'rgb(0, 55, 255)';
+		ctx.fillStyle = 'rgb(241, 241, 241)';
 		ctx.fill();
 	}
 	
@@ -154,7 +139,6 @@ window.onload = function () {
 		ctx.fillText("Track X: " + trackPos.x + " | Track Y: " + trackPos.y, 5, 20);
 		ctx.fillText("Stick X: " + stickPos.x + " | Stick Y: " + stickPos.y, 5, 30);
 		ctx.fillText("Pad X:   " + padPos.x + " | Pad Y:   " + padPos.y, 5, 40);
-		
 		ctx.fillText("OUTPUT", 5, 90);
 		ctx.fillText("X: " + (stickPos.x - padPos.x), 5, 100);
 		ctx.fillText("Y: " + (stickPos.y - padPos.y), 5, 110);
@@ -174,21 +158,21 @@ window.onload = function () {
 		// Checks if there are positions available for the pad and stick
 		if(padPos && stickPos && trackPos){
 		
-			// This is going to move the pad around in case the user has gone really far out of the pad area, it also prevents the stick from some odd behavior. Try commenting out the stickPos updates in this minirithm
+			// This moves the pad around with the user, it also prevents the stick from some odd behavior. Try commenting out the stickPos updates in this minirithm
 			if((trackPos.x - stickRad) < (padPos.x - padRad)){
-				padPos.x = (padPos.x - followSpeed);
+				padPos.x = (trackPos.x + stickRad);
 				stickPos.x = trackPos.x;
 			}
 			if((trackPos.x + stickRad) > (padPos.x + padRad)){
-				padPos.x = (padPos.x + followSpeed);
+				padPos.x = (trackPos.x - stickRad);
 				stickPos.x = trackPos.x;
 			}
 			if((trackPos.y - stickRad) < (padPos.y - padRad)){
-				padPos.y = (padPos.y - followSpeed);
+				padPos.y = (trackPos.y + stickRad);
 				stickPos.y = trackPos.y;
 			}
 			if((trackPos.y + stickRad) > (padPos.y + padRad)){
-				padPos.y = (padPos.y + followSpeed);
+				padPos.y = (trackPos.y - stickRad);
 				stickPos.y = trackPos.y;
 			}
 			
@@ -208,7 +192,6 @@ window.onload = function () {
 			
 			drawJSPad();
 			drawJSStick();
-			drawFollowDot();
 			feedback();
 			
 		}
